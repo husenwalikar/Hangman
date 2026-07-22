1d619bfdaeaa67ef6f9ccc976a73071df74802fe
husenwalikar <husencode@gmail.com>
2026-07-22 23:57:18 +0530

    2nd Commit

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ word_choosen = random.choice(word_list)
 print(word_choosen)
 for i in word_list:
     print(f"'{i}'", end=", ")
-n = 2
+n = 8
+letter_choosen = []
 for i in range(n):
-    word = input(f"Guess the word!: ")
+    letter = input(f"Input a letter: ")
+    letter_choosen += letter
+    if letter_choosen[i] in word_choosen:
+        for i in range(len(word_choosen)):
+            if letter_choosen[i] == word_choosen[i]:
+                print(word_choosen[i], end="")
+                continue
+            else:
+                print("-", end="")
 
-    if word == word_choosen:
-        print("You survived!")
-        break
     else:
-        print("Try Again!")
-        print(f"->{word_choosen[0:3]}{"_"*(len(word_choosen)-3)}")
+        continue
+
 else:
-    print("You Lost!")
+    print("Thanks for playing!")
